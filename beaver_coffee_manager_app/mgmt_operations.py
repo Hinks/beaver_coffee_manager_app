99311df121cb5832485176a3bf71094b3cfb10f4
Hinks <henrik.nr10@gmail.com>
2017-07-17 18:15:57 +0200

    Added some module documentation. removed repeated select...-methods and added a BUISNESS_LOGIC_LAYER data structure holding the different operations a branch manager can perform.

--- beaver_coffee_manager_app/mgmt_operations.py
+++ beaver_coffee_manager_app/mgmt_operations.py
@@ -1,10 +1,19 @@
+"""The purpose of this module is to supply functions that gets executed
+at particular states in the finite-state-machine concerning buisness-logic
+operations like creating statistics reports,
+change employee/customer data and update stock quantities.
+"""
 from pprint import pprint
 import print_utils
 
+import report.type
+import cru.employee
+import cru.customer
+import cru.stock
 
-def select_type_of_report(choice):
-    import report.type
-    reports = {
+
+BUISNESS_LOGIC_LAYER = {
+    'report_operations': {
         '1': report.type.report_1,
         '2': report.type.report_2,
         '3': report.type.report_3,
@@ -12,74 +21,82 @@ def select_type_of_report(choice):
         '5': report.type.report_5,
         '6': report.type.report_6,
         '7': report.type.report_7
+    },
+    'cru_employee': {
+        '1': cru.employee.create,
+        '2': cru.employee.read,
+        '3': cru.employee.update
+    },
+    'cru_customer': {
+        '1': cru.customer.create,
+        '2': cru.customer.read,
+        '3': cru.customer.update
+    },
+    'cru_stock': {
+        '1': cru.stock.read,
+        '2': cru.stock.update
     }
-    return reports[choice]
+}
 
 
 def make_report(db, shop_id):
     print_utils.report_instructions()
 
-    choice = input('\nType the number of the action to perform: ') or '1'
-    action = select_type_of_report(choice)
-    report_result = action(db, shop_id)
-    print(report_result)
-    print_utils.instructions_after_made_report()
+    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('report_operations', None)
+    choice = input('\nType the number of the action to perform: ')
+    context_operation = buisness_logic_context.get(choice, None)
 
-
-def select_employee_operation(choice):
-    import cru.employee
-    operations = {
-        '1': cru.employee.create,
-        '2': cru.employee.read,
-        '3': cru.employee.update
-    }
-    return operations[choice]
+    if context_operation:
+        statistics_report = context_operation(db, shop_id)
+        print(statistics_report)
+        print_utils.instructions_after_made_report()
+    else:
+        print(f'That context operation does not exist: {choice}')
+        print_utils.instructions_after_made_report()
 
 
 def cru_employee(db, shop_id):
     print_utils.instructions_cru_employee()
 
-    choice = input('\nType the number of the action to perform: ') or '1'
-    action = select_employee_operation(choice)
-    result = action(db, shop_id)
-    print(result)
-    print_utils.instructions_after_cru_employee()
+    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_employee', None)
+    choice = input('\nType the number of the action to perform: ')
+    context_operation = buisness_logic_context.get(choice, None)
 
-
-def select_customer_operation(choice):
-    import cru.customer
-    operations = {
-        '1': cru.customer.create,
-        '2': cru.customer.read,
-        '3': cru.customer.update
-    }
-    return operations[choice]
+    if context_operation:
+        response = context_operation(db, shop_id)
+        print(response)
+        print_utils.instructions_after_cru_employee()
+    else:
+        print(f'That context operation does not exist: {choice}')
+        print_utils.instructions_after_cru_employee()
 
 
 def cru_customer(db, shop_id):
     print_utils.instructions_cru_customer()
 
-    choice = input('\nType the number of the action to perform: ') or '1'
-    action = select_customer_operation(choice)
-    result = action(db)
-    print(result)
-    print_utils.instructions_after_cru_customer()
+    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_customer', None)
+    choice = input('\nType the number of the action to perform: ')
+    context_operation = buisness_logic_context.get(choice, None)
 
-
-def select_stock_operation(choice):
-    import cru.stock
-    operations = {
-        '1': cru.stock.read,
-        '2': cru.stock.update
-    }
-    return operations[choice]
-
+    if context_operation:
+        response = context_operation(db)
+        print(response)
+        print_utils.instructions_after_cru_customer()
+    else:
+        print(f'That context operation does not exist: {choice}')
+        print_utils.instructions_after_cru_customer()
 
 def cru_stock(db, shop_id):
     print_utils.instructions_cru_stock()
 
-    choice = input('\nType the number of the action to perform: ') or '1'
-    action = select_stock_operation(choice)
-    result = action(db, shop_id)
-    pprint(result)
-    print_utils.instructions_after_cru_stock()
+    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_stock', None)
+    choice = input('\nType the number of the action to perform: ')
+    context_operation = buisness_logic_context.get(choice, None)
+
+    if context_operation:
+        response = context_operation(db, shop_id)
+        print(response)
+        print_utils.instructions_after_cru_stock()
+    else:
+        print(f'That context operation does not exist: {choice}')
+        print_utils.instructions_after_cru_stock()
