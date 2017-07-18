f20fe76586fb5bbb6815a79b07cbc597f81471ce
Hinks <henrik.nr10@gmail.com>
2017-07-18 11:14:19 +0200

    renamed some states, and changed the mgmt functions to a more automated function which performs the right mgnt operation by reading the what the next state is.

--- beaver_coffee_manager_app/rules.py
+++ beaver_coffee_manager_app/rules.py
@@ -1,44 +1,44 @@
-from mgmt_operations import (make_report, cru_employee, cru_customer, cru_stock)
+from mgmt_operations import (buisness_logic)
 import print_utils
 from auth.auth import logout
 
 
 BRANCH_MGMT_TRANSITIONS = {
-    'start_screen': {'1': {'next_state': 'reports_mod',  'supplied_func': make_report},
-                     '2': {'next_state': 'employee_mod', 'supplied_func': cru_employee},
-                     '3': {'next_state': 'customer_mod', 'supplied_func': cru_customer},
-                     '4': {'next_state': 'stock_mod',    'supplied_func': cru_stock},
+    'start_screen': {'1': {'next_state': 'statistics',   'supplied_func': buisness_logic},
+                     '2': {'next_state': 'cru_employee', 'supplied_func': buisness_logic},
+                     '3': {'next_state': 'cru_customer', 'supplied_func': buisness_logic},
+                     '4': {'next_state': 'cru_stock',    'supplied_func': buisness_logic},
                      '8': {'next_state': 'start_screen', 'supplied_func': logout},
                      '9': {'next_state': 'exit',         'supplied_func': lambda: True}
                      },
 
-    'reports_mod': {'1': {'next_state': 'reports_mod',    'supplied_func': make_report},
-                    '2': {'next_state': 'start_screen',   'supplied_func': print_utils.branch_m_start_screen}
+    'statistics': {'1': {'next_state': 'statistics',    'supplied_func': buisness_logic},
+                    '2': {'next_state': 'start_screen',   'supplied_func': print_utils.start_screen}
                     },
 
-    'employee_mod': {'1': {'next_state': 'employee_mod', 'supplied_func': cru_employee},
-                     '2': {'next_state': 'start_screen', 'supplied_func': print_utils.branch_m_start_screen}
+    'cru_employee': {'1': {'next_state': 'cru_employee', 'supplied_func': buisness_logic},
+                     '2': {'next_state': 'start_screen', 'supplied_func': print_utils.start_screen}
                      },
 
-    'customer_mod': {'1': {'next_state': 'customer_mod', 'supplied_func': cru_customer},
-                     '2': {'next_state': 'start_screen', 'supplied_func': print_utils.branch_m_start_screen}
+    'cru_customer': {'1': {'next_state': 'cru_customer', 'supplied_func': buisness_logic},
+                     '2': {'next_state': 'start_screen', 'supplied_func': print_utils.start_screen}
                      },
 
-    'stock_mod': {'1': {'next_state': 'stock_mod',    'supplied_func': cru_stock},
-                  '2': {'next_state': 'start_screen', 'supplied_func': print_utils.branch_m_start_screen}
+    'cru_stock': {'1': {'next_state': 'cru_stock',    'supplied_func': buisness_logic},
+                  '2': {'next_state': 'start_screen', 'supplied_func': print_utils.start_screen}
                   }
 }
 
 
 SALES_MGMT_TRANSITIONS = {
-    'start_screen': {'1': {'next_state': 'reports_mod',  'supplied_func': make_report},
+    'start_screen': {'1': {'next_state': 'statistics',   'supplied_func': buisness_logic},
                      '8': {'next_state': 'start_screen', 'supplied_func': logout},
                      '9': {'next_state': 'exit',         'supplied_func': lambda: True}
                      },
 
-    'reports_mod': {'1': {'next_state': 'reports_mod',    'supplied_func': make_report},
-                    '2': {'next_state': 'start_screen',   'supplied_func': print_utils.sales_m_start_screen}
-                    }
+    'statistics': {'1': {'next_state': 'statistics',    'supplied_func': buisness_logic},
+                   '2': {'next_state': 'start_screen',  'supplied_func': print_utils.start_screen}
+                   }
 }
 
 
@@ -47,5 +47,9 @@ default_app_data = {
     'branch_mgmt_rules': BRANCH_MGMT_TRANSITIONS,
     'sales_mgmt_rules': SALES_MGMT_TRANSITIONS,
     'state': 'start_screen',
-    'session': {'is_authenticated': False, 'shop_id': None}
+    'session': {
+        'is_authenticated': False,
+        'logged_in_as': None,
+        'shop_id': None
+    }
 }
