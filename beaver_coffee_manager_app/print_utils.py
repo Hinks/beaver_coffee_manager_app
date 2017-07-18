0def5d6bc54e4026511a9dbe7e6a77057a78fda2
Hinks <henrik.nr10@gmail.com>
2017-07-18 11:21:59 +0200

    added new functions that will return the right print function depending on what the user is logged in as or in which state the application is currently in.

--- beaver_coffee_manager_app/print_utils.py
+++ beaver_coffee_manager_app/print_utils.py
@@ -1,5 +1,33 @@
+def start_screen(logged_in_as):
+    screens = {
+        'branch_manager': branch_m_start_screen,
+        'sales_manager': sales_m_start_screen
+    }
+    return screens[logged_in_as]
+
+
+def bll_instructions(next_state):
+    instructions = {
+        'statistics': instructions_statistics,
+        'cru_employee': instructions_cru_employee,
+        'cru_customer': instructions_cru_customer,
+        'cru_stock': instructions_cru_stock
+    }
+    return instructions[next_state]
+
+
+def instructions_after(next_state):
+    instructions = {
+        'statistics': instructions_after_statistics,
+        'cru_employee': instructions_after_cru_employee,
+        'cru_customer': instructions_after_cru_customer,
+        'cru_stock': instructions_after_cru_stock
+    }
+    return instructions[next_state]
+
+
 def branch_m_start_screen():
-    msg = '\n{}\n{}\n{}\n{}\n{}\n{}'.format(
+    msg = '\nBM: Start screen:\n{}\n{}\n{}\n{}\n{}\n{}'.format(
         '1. Generate Reports',
         '2. Create,Read,Update Employee data',
         '3. Create,Read,Update Customer data',
@@ -8,15 +36,16 @@ def branch_m_start_screen():
         '9. Exit')
     print(msg)
 
+
 def sales_m_start_screen():
-    msg = '\n{}\n{}\n{}'.format(
+    msg = '\nSM: Start screen:\n{}\n{}\n{}'.format(
         '1. Generate Reports',
         '8. Logout',
         '9. Exit')
     print(msg)
 
-#---------------Print methods for mgnt-operations------------------
-def report_instructions():
+
+def instructions_statistics():
     instructions = '\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(
         '1. Sales for any specified time period',
         '2. Sales for of one or more products over a time period',
@@ -28,10 +57,19 @@ def report_instructions():
     print(instructions)
 
 
-def instructions_after_made_report():
-    msg = '\n{}\n{}'.format(
-        '1. Generate Reports',
-        '2. Start screen')
+def instructions_cru_employee():
+    msg = '\n{}\n{}\n{}'.format(
+        '1. Create new employee',
+        '2. Read employee',
+        '3. Update employee')
+    print(msg)
+
+
+def instructions_cru_customer():
+    msg = '\n{}\n{}\n{}'.format(
+        '1. Create new customer',
+        '2. Read customer',
+        '3. Update customer')
     print(msg)
 
 
@@ -42,6 +80,13 @@ def instructions_cru_stock():
     print(msg)
 
 
+def instructions_after_statistics():
+    msg = '\n{}\n{}'.format(
+        '1. Generate Reports',
+        '2. Start screen')
+    print(msg)
+
+
 def instructions_after_cru_stock():
     msg = '\n{}\n{}'.format(
         '1. Create, Read, Update stock',
@@ -49,26 +94,10 @@ def instructions_after_cru_stock():
     print(msg)
 
 
-def instructions_cru_employee():
-    msg = '\n{}\n{}\n{}'.format(
-        '1. Create new employee',
-        '2. Read employee',
-        '3. Update employee')
-    print(msg)
-
-
 def instructions_after_cru_employee():
     msg = '\n{}\n{}'.format(
         '1. Create, Read, Update employee',
         '2. Start screen')
-    print(msg)
-
-
-def instructions_cru_customer():
-    msg = '\n{}\n{}\n{}'.format(
-        '1. Create new customer',
-        '2. Read customer',
-        '3. Update customer')
     print(msg)
 
 
