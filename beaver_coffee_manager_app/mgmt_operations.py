9e8071d8d92a0049c1a7e1549e860ea0680ca5f1
Hinks <henrik.nr10@gmail.com>
2017-07-18 11:28:13 +0200

    Added a more generic buisness logic function that will call the right buisness logic operations and print instructions depening on what the next state is.

--- beaver_coffee_manager_app/mgmt_operations.py
+++ beaver_coffee_manager_app/mgmt_operations.py
@@ -6,21 +6,21 @@ change employee/customer data and update stock quantities.
 from pprint import pprint
 import print_utils
 
-import report.type
+import statistics.reports
 import cru.employee
 import cru.customer
 import cru.stock
 
 
 BUISNESS_LOGIC_LAYER = {
-    'report_operations': {
-        '1': report.type.report_1,
-        '2': report.type.report_2,
-        '3': report.type.report_3,
-        '4': report.type.report_4,
-        '5': report.type.report_5,
-        '6': report.type.report_6,
-        '7': report.type.report_7
+    'statistics': {
+        '1': statistics.reports.report_1,
+        '2': statistics.reports.report_2,
+        '3': statistics.reports.report_3,
+        '4': statistics.reports.report_4,
+        '5': statistics.reports.report_5,
+        '6': statistics.reports.report_6,
+        '7': statistics.reports.report_7
     },
     'cru_employee': {
         '1': cru.employee.create,
@@ -39,64 +39,32 @@ BUISNESS_LOGIC_LAYER = {
 }
 
 
-def make_report(db, shop_id):
-    print_utils.report_instructions()
+def function_arity(fun):
+    import inspect
+    return len(inspect.getargspec(fun)[0])
 
-    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('report_operations', None)
+
+def buisness_logic(db, next_state, user_session):
+    print_utils.bll_instructions(next_state)()
+
+    buisness_logic_context = BUISNESS_LOGIC_LAYER.get(next_state, None)
     choice = input('\nType the number of the action to perform: ')
     context_operation = buisness_logic_context.get(choice, None)
 
     if context_operation:
-        statistics_report = context_operation(db, shop_id)
-        print(statistics_report)
-        print_utils.instructions_after_made_report()
+        # Match what to do depending on the supplied function's arity
+        #This matches the cru.customer functions in the buisness_logic_layer
+        if function_arity(context_operation) is 1:
+            result = context_operation(db)
+            print(result)
+            print_utils.instructions_after(next_state)()
+
+        #This matches the functions in the buisness_logic_layer
+        #
+        elif function_arity(context_operation) is 2:
+            result = context_operation(db, user_session['shop_id'])
+            print(result)
+            print_utils.instructions_after(next_state)()
     else:
         print(f'That context operation does not exist: {choice}')
-        print_utils.instructions_after_made_report()
-
-
-def cru_employee(db, shop_id):
-    print_utils.instructions_cru_employee()
-
-    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_employee', None)
-    choice = input('\nType the number of the action to perform: ')
-    context_operation = buisness_logic_context.get(choice, None)
-
-    if context_operation:
-        response = context_operation(db, shop_id)
-        print(response)
-        print_utils.instructions_after_cru_employee()
-    else:
-        print(f'That context operation does not exist: {choice}')
-        print_utils.instructions_after_cru_employee()
-
-
-def cru_customer(db, shop_id):
-    print_utils.instructions_cru_customer()
-
-    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_customer', None)
-    choice = input('\nType the number of the action to perform: ')
-    context_operation = buisness_logic_context.get(choice, None)
-
-    if context_operation:
-        response = context_operation(db)
-        print(response)
-        print_utils.instructions_after_cru_customer()
-    else:
-        print(f'That context operation does not exist: {choice}')
-        print_utils.instructions_after_cru_customer()
-
-def cru_stock(db, shop_id):
-    print_utils.instructions_cru_stock()
-
-    buisness_logic_context = BUISNESS_LOGIC_LAYER.get('cru_stock', None)
-    choice = input('\nType the number of the action to perform: ')
-    context_operation = buisness_logic_context.get(choice, None)
-
-    if context_operation:
-        response = context_operation(db, shop_id)
-        print(response)
-        print_utils.instructions_after_cru_stock()
-    else:
-        print(f'That context operation does not exist: {choice}')
-        print_utils.instructions_after_cru_stock()
+        print_utils.instructions_after(next_state)()
