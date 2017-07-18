3fe45e2b0cb9a76792cccc492b3f8e559cc3de83
Hinks <henrik.nr10@gmail.com>
2017-07-18 11:24:03 +0200

    Added more match clauses in the accept function, so the supplied functions gets called with the right parameters.

--- beaver_coffee_manager_app/fsm.py
+++ beaver_coffee_manager_app/fsm.py
@@ -1,5 +1,6 @@
 from toolz.dicttoolz import assoc
 from auth.auth import authenticate
+import print_utils
 
 
 def function_arity(fun):
@@ -11,40 +12,47 @@ def accept(app_data, key, db):
     """ Jumps to the next state that is specified in the rules from
     app_data['current_rules'] and runs the supplied function.
     """
+    try:
+        current_state = app_data['state']
+        new_state = app_data['current_rules'][current_state][key].get('next_state')
+        supplied_func = app_data['current_rules'][current_state][key].get('supplied_func')
 
-    current_state = app_data['state']
-    new_state = app_data['current_rules'][current_state][key].get('next_state')
-    supplied_func = app_data['current_rules'][current_state][key].get('supplied_func')
+        session = app_data['session']
 
-    session = app_data['session']
+        # Match what to do depending on the supplied function's arity
+        #This matches the lambda function in the rules module
+        if function_arity(supplied_func) is 0:
+            supplied_func()
+            return assoc(app_data, 'state', new_state)
 
-    # Match what to do depending on the supplied function's arity
+        #This matches the print_utils.start_screen function in the rules module
+        elif function_arity(supplied_func) == 1:
+            supplied_func(session['logged_in_as'])()
+            return assoc(app_data, 'state', new_state)
 
-    # If the supplied function has zero argument, the function(usually a print method)
-    # is called before returning the app_data with the next state set.
-    if function_arity(supplied_func) == 0:
-        supplied_func()
-        return assoc(app_data, 'state', new_state)
-    # If the supplied function has one argument, the app_data gets updated by the
-    # supplied function and then returned. This match is only matched when the user
-    # logs out and then the supplied logout function clear the session in app_data.
-    elif function_arity(supplied_func) == 1:
-        return supplied_func(assoc(app_data, 'state', new_state))
-    # If the supplied function has two arguments, the mgmt operations are called.
-    # They expect the db connection as the first argument and the shop_id as the
-    # second.
-    elif function_arity(supplied_func) == 2:
-        supplied_func(db, session.get('shop_id'))
-        return assoc(app_data, 'state', new_state)
-    # Just in case nothing matches, app_data is returned in the state it came in with.
-    else:
-        return app_data
+        #This matches the logout function in the rules module
+        elif function_arity(supplied_func) == 2:
+            return supplied_func(db, assoc(app_data, 'state', new_state))
+
+        #This matches the buisness_logic function in the rules module
+        elif function_arity(supplied_func) == 3:
+            supplied_func(db, new_state, session)
+            return assoc(app_data, 'state', new_state)
+
+        # Just in case nothing matches, app_data is returned in the state it came in with.
+        else:
+            return app_data
+    except KeyError as e:
+        print(f'{key} is not a valid option.')
+        print_utils.start_screen(app_data['session']['logged_in_as'])()
+        return  assoc(app_data, 'state', 'start_screen')
+
 
 
 def run_next(db, app_data):
     """ This is the application loop. It stops and closes the db connection
     then the state is set to: exit. The user has to sign in before he/she can enter
-    the "finite state machine". 
+    the "finite state machine".
     """
     if app_data['state'] == 'exit':
         db.client.close()
